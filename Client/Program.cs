5074114a32e264b6d08e9b95cbd824fb44ec8c8d
Cozma Daniel <cozma.daniel99@gmail.com>
2020-04-14 00:16:20 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Host = "localhost";
+            const int Port = 16973;
+
+            var channel = new Channel($"{Host}:{Port}", ChannelCredentials.Insecure);
+
+            Console.WriteLine("Numele tau: ");
+
+            do
+            {
+                var key = Console.ReadLine();
+
+                var client = new Generated.ServiciuCerereNume.ServiciuCerereNumeClient(channel);
+
+
+                client.AfiseazaNume(new Generated.Nume
+                {Nume_ = key});
+            }
+
+            while (true);
+
+        }
+    }
+}
---
+++ Server/ServiciuDePrimireANumelui.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Core;
+using Generated;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class ServiciuDePrimireANumelui : Generated.ServiciuCerereNume.ServiciuCerereNumeBase
+    {
+
+        public override Task<Gol> AfiseazaNume(Nume request, ServerCallContext context)
+        {
+            System.Console.WriteLine("Hello {0}", request.Nume_ + "!");
+           
+            return Task.FromResult(new Gol() { });
+        }
+    }
+}
